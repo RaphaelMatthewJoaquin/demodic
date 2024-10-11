33510a89081c85b6d969913c5f237f00630045e5
RaphaelMatthewJoaquin <157200258+RaphaelMatthewJoaquin@users.noreply.github.com>
2024-10-10 19:27:14 -0800

    Add files via upload

---
+++ dictionary/dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var months = new Dictionary<int, string>
+            {
+                {1, "JANUARY" },
+                { 2, "FEB" },
+                { 3, "MARCH" },
+                { 4, "APRIL" },
+                
+            };
+
+            months[5] = "may";
+            months[6] = "june";
+            months[7] = "july";
+            months[8] = "august";
+
+
+            months.Add(9,"september");
+            months.Add(10,"OCT");
+            months.Add(11,"NOV");
+            months.Add(12,"DEC");
+
+            foreach(var s in months)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dictionaryrealworld/dictionaryrealworld/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionaryrealworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees =  
+            {
+                new Employee("CEO", "MATTHEW", 20, 100000),
+                new Employee("MANAGER", "MATTHEW", 21, 400),
+                new Employee("HR", "MATTHEW", 22, 1170),
+                new Employee("LEAD DEVELOPER", "MATTHEW", 23, 277353),
+                new Employee("INTERN", "MATTHEW", 24, 6900),
+            };
+
+            Dictionary<string, Employee> employeedic = new Dictionary<string, Employee>();
+
+            foreach (Employee emp in employees)
+            {
+                employeedic.Add(emp.Role, emp);
+            }
+
+            foreach(Employee emp in employeedic.Values) 
+            {
+                Console.Write(" EMPLOYEE NAME: {0}", emp.Name);
+                Console.Write(" EMPLOYEE ROLE: {0}", emp.Role);
+                Console.Write(" EMPLOYEE AGE: {0}", emp.Age);
+                Console.WriteLine(" EMPLOYEE SALARY: {0}", emp.Salary);
+                Console.WriteLine("-----------------------------------------------------------");
+
+            }
+
+
+            /*string key = string.Empty;
+
+            while(key != "N" )
+            {
+
+
+                Console.WriteLine("CONTAINS SAMPLE");
+
+                key = Console.ReadLine();
+
+                if (employeedic.ContainsKey(key))
+                {
+                    Employee emp = employeedic[key];
+
+                    Console.WriteLine("EMPLOYEE NAME: {0}, ROLE: {1}, SALARY: {2}", emp.Name, emp.Role, emp.Salary);
+                }
+                else
+                {
+                    Console.WriteLine("NO EMPLOYEE FOUND");
+                }*/
+
+
+            string keytoupdate = "CEO";
+            
+
+            if (employeedic.ContainsKey(keytoupdate))
+            {
+                employeedic[keytoupdate] = new Employee("CEO", "DANTE", 117, 1);
+                Console.WriteLine("DUE TO SOME VIOLATION OF CONTRACT BY EMPLOYEE ");
+   
+                Console.WriteLine("AND WAS REPLACED WITH {0}", keytoupdate);
+
+            }
+            else
+            {
+                Console.WriteLine("NO EMPLOYEE HAS BEEN FOUND WITH THIS KEY {0}", keytoupdate);
+            }
+
+            foreach (Employee emp in employeedic.Values)
+            {
+                Console.Write(" EMPLOYEE NAME: {0}", emp.Name);
+                Console.Write(" EMPLOYEE ROLE: {0}", emp.Role);
+                Console.Write(" EMPLOYEE AGE: {0}", emp.Age);
+                Console.WriteLine(" EMPLOYEE SALARY: {0}", emp.Salary);
+                Console.WriteLine("-----------------------------------------------------------");
+
+            }
+
+            string keytoremove = "INTERN";
+
+            if (employeedic.Remove(keytoremove))
+            { 
+                Console.WriteLine("EMPLOYEE {0} HAS BEEN TERMINATED", keytoremove);
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE NOT FOUND");
+            }
+
+
+            foreach (Employee emp in employeedic.Values)
+            {
+                Console.Write(" EMPLOYEE NAME: {0}", emp.Name);
+                Console.Write(" EMPLOYEE ROLE: {0}", emp.Role);
+                Console.Write(" EMPLOYEE AGE: {0}", emp.Age);
+                Console.WriteLine(" EMPLOYEE SALARY: {0}", emp.Salary);
+                Console.WriteLine("-----------------------------------------------------------");
+
+            }
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
